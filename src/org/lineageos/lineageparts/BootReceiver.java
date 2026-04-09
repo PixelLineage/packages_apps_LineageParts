a899cbbce4bc20067ce5d0398b51dd06dcecdd0f
Quick ⚡ <quick@disroot.org>
2026-04-10 02:14:17 +0530

    fixup! Drop Profiles and Contributors completely

--- src/org/lineageos/lineageparts/BootReceiver.java
+++ src/org/lineageos/lineageparts/BootReceiver.java
@@ -26,9 +26,6 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
-        // Extract the contributors database
-        ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);
-
         // Toggle visibility of some settings regardless of user type
         LiveDisplaySettings.restoreLiveDisplay(ctx);
         TouchscreenGestureSettings.restoreTouchscreenGestures(ctx);
